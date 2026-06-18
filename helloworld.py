16cbc4a0a04664925851c746a6f5af563070c16c
EUNGWANCHOI <silvergwan@gmail.com>
2026-06-18 15:10:14 +0900

    init: hello world

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# 1일차 - 30-Days-Of-Python Challenge
+
+print(2 + 3)             # 더하기 연산+)
+print(3 - 1)             # 빼기 연산(-)
+print(2 * 3)             # 곱하기 연산(*)
+print(3 / 2)             # 나누기 연산 - 실수(float)로 반환(/)
+print(3 ** 2)            # 제곱 연산(**)
+print(3 % 2)             # 나머지 연산(%)
+print(3 // 2)            # 몫 연산(//)
+
+# 타입 확인
+print(type(10))          # 정수
+print(type(3.14))        # 실수
+print(type(1 + 3j))      # (복소)수
+print(type('Asabeneh'))  # 문자열
+print(type([1, 2, 3]))   # 리스트
+print(type({'name':'Asabeneh'})) # 딕셔너리(key:value)
+print(type({9.8, 3.14, 2.7}))    # 셋(중복값허용X)
+print(type((9.8, 3.14, 2.7)))    # 튜플(Immutable)
