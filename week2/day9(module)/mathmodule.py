b1709fee8294e819f759f7130f10b2de09ac2fe3
EUNGWANCHOI <silvergwan@gmail.com>
2026-06-23 14:58:34 +0900

    feat: simple module(math, random)

---
+++ week2/day9(module)/mathmodule.py
@@ -0,0 +1,5 @@
+import math
+
+print(math.pi)
+print(math.sqrt(144))
+print(math.ceil(4.2))
---
+++ week2/day9(module)/randommodule.py
@@ -0,0 +1,8 @@
+import random
+
+numbers = [1, 2, 3, 4, 5]
+
+print(random.randint(1, 10))
+
+random.shuffle(numbers)
+print(numbers)
