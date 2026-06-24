62563d2cda9c9a60edcd7e8638dbf6b5ac206039
EUNGWANCHOI <silvergwan@gmail.com>
2026-06-24 11:03:42 +0900

    chore: file dir change

---
+++ day11(numpy)/test1.py
@@ -0,0 +1,25 @@
+'''
+
+과제 1 — 배열 만들기
+
+import numpy as np
+
+# 1. 리스트로 1차원 배열 만들기
+# [1, 2, 3, 4, 5]를 np.array()로 변환하고 출력
+
+# 2. 0으로 채워진 (3, 4) 배열 만들기
+# 힌트: np.zeros()
+
+# 3. 1로 채워진 (2, 3) 배열 만들기
+# 힌트: np.ones()
+
+# 4. 0부터 9까지 숫자로 채워진 배열 만들기
+# 힌트: np.arange()
+
+'''
+
+import numpy as np
+
+## 1
+numbers = np.array([1, 2, 3, 4, 5])
+print(numbers)
